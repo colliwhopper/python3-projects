884a4b1c749b266b356013a0861996191f2f87b3
James MacColl <info@gabrielit.co.uk>
2022-07-04 19:11:33 +0100

    tip calculator - always show x2 decimal places

--- tip_calculator.py
+++ tip_calculator.py
@@ -2,10 +2,10 @@
 
 print("Welcome to the tip calculator!")
 
-total = float( input("What was the total bill? ") )
-tip = int( input("How much tip would you like to give? ") )
+total = float( input("What was the total bill? £") )
+tip = int( input("What % would you like to tip? ") )
 split = int ( input("How many people to split the bill? ") )
-
 final_total = round( (total + (total/tip)) / split, 2 )
+final_total = "{:.2f}".format(final_total)
 
-print(f"Each person should pay: {final_total}")
+print(f"Each person should pay: £{final_total}")
