82c473153bce3536f2c99576007eea5230873a88
James MacColl <info@gabrielit.co.uk>
2022-07-09 18:27:37 +0100

    typo

--- who_pays_game.py
+++ who_pays_game.py
@@ -2,13 +2,13 @@ import random
 
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-players = int((len(names)))
+players = int((len(names[ -1])))
 payer = print( names[random.randint(0, int(players))] + " is going to buy the meal today!" )
 
 
 #nasty 2-line version:
 # names_string = input("Give me everybody's names, separated by a comma. ")
-# payer = print( names_string.split(", ")[random.randint(0, int((len(names_string.split(", ")))))] + " is going to buy the meal today!" )
+# payer = print( names_string.split(", ")[random.randint(0, int((len(names_string[ -1].split(", ")))))] + " is going to buy the meal today!" )
 
 #using the random module "choice" parameter:
 # import random
