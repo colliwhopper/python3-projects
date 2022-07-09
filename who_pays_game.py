ff3a0d7c94b74341b6e28db4d8ce51d41c4c348d
James MacColl <info@gabrielit.co.uk>
2022-07-09 18:15:58 +0100

    who pays game additional

--- who_pays_game.py
+++ who_pays_game.py
@@ -9,3 +9,12 @@ payer = print( names[random.randint(0, int(players))] + " is going to buy the me
 #nasty 2-line version:
 # names_string = input("Give me everybody's names, separated by a comma. ")
 # payer = print( names_string.split(", ")[random.randint(0, int((len(names_string.split(", ")))))] + " is going to buy the meal today!" )
+
+#using the random module "choice" parameter:
+# import random
+#
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
+# payer = random.choice(names)
+#
+# print( payer + " is going to buy the meal today!" )
